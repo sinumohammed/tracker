c17d9ee49ed91e57fe12e6d038199d22b4892153
unknown <you@example.comsinumohammed@gmail.com>
2018-03-06 09:40:24 +0530

    clent hit server demo

---
+++ client/src/components/Register.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+    type='email'
+    placeholder='email'
+    v-model='email'/>
+    <br>
+    <input
+    type='password'
+    placeholder='password'
+    v-model='password'/>
+    <br>
+    <button
+    @click='register'
+    >Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  methods: {
+    async register () {
+      const Response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(Response.data)
+    }
+  }
+  // watch: {
+  //   email (value) {
+  //     console.log('Email changed', value)
+  //   }
+  // },
+  // mounted () {
+  //   setTimeout(() => {
+  //     this.email = 'sinumohammed@gmail.com'
+  //   }, 2000)
+  // }
+}
+
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 console.log('hello')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register', (req, resp) => {
+  resp.send({
+    message: `Hello ${req.body.email}! your user was created. Have fun`
+  })
+})
+
+app.listen(process.env.PORT || 8084)
